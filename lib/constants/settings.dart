4614b4600f6dfb210936b50d39ba97cb1c975812
Mohammed Waseem Anjum <mohammeedwaseem.anjum@gmail.com>
2021-03-11 18:39:20 +0800

    change release value

--- lib/constants/settings.dart
+++ lib/constants/settings.dart
@@ -4,7 +4,7 @@ final Map<String,dynamic> settings = {
   'serverUrl': 'https://zebra.atmosclouds.com/',
   'client_id': 'hw8qFeTezCenWSv82okRYCW5dcT0n7NVgzFPq6ax',
   'client_secret': 'pQ9pLd5pLEnRnBJRUAShVgsmfGuO9lXDaZ8UQUH3uLOww8sun6q6oGyM9f85D4KwxrW5CqfBtnaxODkLo8fv0c32lC9vHqcyOWt3w0ryDCX6maak7iDvoRYeNDLb63lu',
-  'isRelease':false,
+  'isRelease':true,
 };
 
 
